05afd8df616dc56eb2871fb18962e49ec0fb5444
Tim <chent1101@gmail.com>
2015-10-27 23:27:53 -0600

    parsed promted variable to be a number

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var number = prompt("Yo! Enter your number to initiate FizzBuzz, homie!")
-    for (var i = 1; i < number + 1; i += 1) {
+    var numberAndOne = parseInt(number);
+    for (var i = 1; i < numberAndOne + 1; i += 1) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
             $('body').append('<p>FizzBuzz</p>');
         } else if (i % 3 == 0) {
