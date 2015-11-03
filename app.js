1d27172498a3b968a5888802a5c1095ddb7635d6
Tim <chent1101@gmail.com>
2015-11-02 20:50:53 -0700

    refactored

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
     var number = prompt("Yo! Enter your number to initiate FizzBuzz, homie!")
-    var numberAndOne = parseInt(number);
-    for (var i = 1; i < numberAndOne + 1; i += 1) {
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            $('body').append('<p>FizzBuzz</p>');
-        } else if (i % 3 == 0) {
-            $('body').append('<p>Fizz</p>');
-        } else if (i % 5 == 0) {
-            $('body').append('<p>Buzz</p>');
-        } else {
-            $('body').append('<p>' + i + '</p>');
-        }
+
+    function fizzbuzz(number) {
+        var number1 = parseInt(number);
+        for (var i = 1; i < number1 + 1; i += 1) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                $('body').append('<p>FizzBuzz</p>');
+            } else if (i % 3 == 0) {
+                $('body').append('<p>Fizz</p>');
+            } else if (i % 5 == 0) {
+                $('body').append('<p>Buzz</p>');
+            } else {
+                $('body').append('<p>' + i + '</p>');
+            }
+        };
     };
+
+    fizzbuzz(number);
+
 });
